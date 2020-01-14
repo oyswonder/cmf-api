d3ed14b1744e4709ae9c500fe458ff58c65d3803
oyswonder <oyswonder@qq.com>
2020-01-14 18:39:04 +0800

    增加模板，移除异常处理类

--- src/app.php
+++ src/app.php
@@ -136,7 +136,7 @@ return [
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
-    'exception_handle'       => '\api\exception\ApiHandle',
+    'exception_handle'       => '',
 ];
 
 
